a633a64af99b98ce698f4c9516df882b08779f42
Utku Alcan <aalcan.utku@gmail.com>
2024-05-23 21:50:00 +0300

    Project is completed.

--- main.c
+++ main.c
@@ -28,6 +28,7 @@ typedef struct {
 // Function prototypes
 void fcfs_scheduler(Process *processes, int num_processes, FILE *output_file);
 void sjf_scheduler(Process *processes, int num_processes, FILE *output_file);
+void round_robin_scheduler(Process *processes, int num_processes, int quantum, int priority_queue, FILE *output_file);
 void add_process_to_queue(Process *process, Queue *queue);
 Process *remove_process_from_queue(Queue *queue);
 int is_queue_empty(Queue *queue);
@@ -57,6 +58,8 @@ int main() {
     // Perform scheduling
     fcfs_scheduler(processes, num_processes, output_file);
     sjf_scheduler(processes, num_processes, output_file);
+    round_robin_scheduler(processes, num_processes, 8, MEDIUM_PRIORITY_QUEUE, output_file);
+    round_robin_scheduler(processes, num_processes, 16, LOW_PRIORITY_QUEUE, output_file);
 
     fclose(output_file);
 
@@ -132,7 +135,7 @@ void sjf_scheduler(Process *processes, int num_processes, FILE *output_file) {
     // Execute processes in SJF order
     while (!is_queue_empty(&cpu2_queue)) {
         int shortest_index = -1;
-        int shortest_burst_time = INT_MAX;
+        int shortest_burst_time = _INT_MAX_;
         for (int i = cpu2_queue.front; i <= cpu2_queue.rear; i++) {
             if (cpu2_queue.arr[i]->burst_time < shortest_burst_time) {
                 shortest_burst_time = cpu2_queue.arr[i]->burst_time;
@@ -145,12 +148,41 @@ void sjf_scheduler(Process *processes, int num_processes, FILE *output_file) {
             fprintf(output_file, "Process %s is assigned to CPU-2.\n", current_process->name);
             current_time += current_process->burst_time;
             fprintf(output_file, "The operation of process %s is completed and terminated.\n", current_process->name);
-
+            
             // Remove the process from the queue
             for (int i = shortest_index; i < cpu2_queue.rear; i++) {
                 cpu2_queue.arr[i] = cpu2_queue.arr[i + 1];
             }
             cpu2_queue.rear--;
+        }
+    }
+}
+
+void round_robin_scheduler(Process *processes, int num_processes, int quantum, int priority_queue, FILE *output_file) {
+    Queue cpu2_queue = { .front = 0, .rear = -1 };
+    int current_time = 0;
+
+    // Queue up all processes with the given priority
+    for (int i = 0; i < num_processes; i++) {
+        if (processes[i].priority == priority_queue) {
+            fprintf(output_file, "Process %s is placed in the que%d queue to be assigned to CPU-2.\n", processes[i].name, priority_queue == MEDIUM_PRIORITY_QUEUE ? 3 : 4);
+            add_process_to_queue(&processes[i], &cpu2_queue);
+        }
+    }
+
+    // Execute processes in Round Robin order
+    while (!is_queue_empty(&cpu2_queue)) {
+        Process *current_process = remove_process_from_queue(&cpu2_queue);
+        fprintf(output_file, "Process %s is assigned to CPU-2.\n", current_process->name);
+
+        if (current_process->burst_time > quantum) {
+            current_time += quantum;
+            current_process->burst_time -= quantum;
+            fprintf(output_file, "Process %s run until the defined quantum time and is queued again because the process is not completed.\n", current_process->name);
+            add_process_to_queue(current_process, &cpu2_queue);
+        } else {
+            current_time += current_process->burst_time;
+            fprintf(output_file, "Process %s is assigned to CPU-2, its operation is completed and terminated.\n", current_process->name);
         }
     }
 }
@@ -175,5 +207,5 @@ Process *remove_process_from_queue(Queue *queue) {
 }
 
 int is_queue_empty(Queue *queue) {
-    return (queue->front >queue->rear);
+    return (queue->front > queue->rear);
 }
